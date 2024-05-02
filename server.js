9fbd50f2374f9c89b983317a4220f1da5a7fa975
NoB0T21 <nobotgawade96@gmail.com>
2024-05-02 19:09:00 +0530

    'canva-clone'

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const momgoode = require('mongoose')
+const mongoose = require('mongoose')
 const app = express()
 const dotenv = require('dotenv')
 const cors = require('cors')
@@ -7,43 +7,45 @@ const path = require('path')
 
 
 dotenv.config()
+app.use(express.json())
 
-if(process.env.NODE_ENV === 'local') {
+if (process.env.NODE_ENV === 'local') {
     app.use(cors({
-        origin: 'http://localhost:3000',
-        credentials : true
+        origin: 'http://localhost:5173',
+        credentials: true
     }))
-}else{
+} else {
     app.use(cors({
-        credentials : true
+        credentials: true
     }))
 }
 
+app.use('/api', require('./routes/designRoutes'))
+app.use('/api', require('./routes/authRoutes'))
 
-
-
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"./frontend/dist")))
-    app.get('*',(req,res)=> {
-        res.sendFile(path.resolve(__dirname,'./','frontend','dist','index.html'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "./frontend/dist")))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"))
     })
 }
 
-const dbConnect = async()=>{
-    try{
-        if(process.env.NODE_ENV === 'local'){
-            await momgoode.connect(process.env.LOCAL_DB_URI)
-            console.log('Local database is connect....')      
-        }else{
-            await momgoode.connect(process.env.MONGODB_URI)
-            console.log('production database is connect....')    
+const dbConnect = async () => {
+    try {
+        if (process.env.NODE_ENV === 'local') {
+            await mongoose.connect(process.env.LOCAL_DB_URI)
+            console.log('Local database is connect....')
+        } else {
+            await mongoose.connect(process.env.MONGODB_URI)
+            console.log('production database is connect....')
         }
-    } catch (error){
+    } catch (error) {
         console.log('database connection failed')
+    }
 }
-}
+
 dbConnect()
 
 const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
 
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}..`))
